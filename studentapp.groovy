af5a2fb48f62570f6d43825d1e1c277b42ec6c1b
Pallavi-1104 <tarkeshubham2006@gmail.com>
2024-10-15 13:18:02 +0530

    script

--- studentapp.groovy
+++ studentapp.groovy
@@ -5,36 +5,38 @@ pipeline {
     stages {
         stage('Pull') {
             steps {
-                echo "we are pulling from github"
+                echo "We are pulling from GitHub"
                 git "https://github.com/AnupDudhe/studentapp-ui"
             }
         }
         stage('Build') {
             steps {
                 sh '''
-                sudo apt install mvn clean
                 sudo apt update
+                sudo apt install maven -y
+                mvn clean install package
                 sudo apt install unzip -y
                 sudo curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.96/bin/apache-tomcat-9.0.96.zip
                 sudo unzip apache-tomcat-9.0.96.zip
-                echo we are building source code
+                echo "We are building the source code"
                 '''
             }
         }
         stage('Test') {
             steps {
-                sh '''echo "we are tesing"
+                sh '''echo "We are testing"
                 '''
             }
         }
         stage('Deploy') {
             steps {
                 sh '''
-                sudo mv target/*.war  apache-tomcat-9.0.95/webapps/student.war
-                sudo bash apache-tomcat-9.0.95/bin/catalina.sh start
-                echo "we are deploying"
+                sudo mv target/*.war apache-tomcat-9.0.96/webapps/student.war
+                sudo bash apache-tomcat-9.0.96/bin/catalina.sh start
+                echo "We are deploying"
                 '''
             }
         }
     }
 }
+
