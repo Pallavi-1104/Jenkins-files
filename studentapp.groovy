7abf8411bb31e20161ccb6caf4c1f0d2d63734cf
Pallavi-1104 <tarkeshubham2006@gmail.com>
2024-10-17 15:37:15 +0530

    Update studentapp.groovy

--- studentapp.groovy
+++ studentapp.groovy
@@ -17,7 +17,7 @@ pipeline {
                 mvn clean install package
                 sudo apt install unzip -y
                 sudo curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.96/bin/apache-tomcat-9.0.96.zip
-                sudo unzip apache-tomcat-9.0.96.zip
+                sudo unzip -o apache-tomcat-9.0.96.zip
                 echo "We are building the source code"
                 '''
             }
